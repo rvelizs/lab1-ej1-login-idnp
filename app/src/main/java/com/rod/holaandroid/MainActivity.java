e95e6596feaabce4c33f2ee75fd845064d79a543
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-09-18 19:26:51 -0500

    Cambio de identificadores y propiedad pista en los campos de texto

--- app/src/main/java/com/rod/holaandroid/MainActivity.java
+++ app/src/main/java/com/rod/holaandroid/MainActivity.java
@@ -8,7 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.rod.holaandroid.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
