6bad903afd92dde53d13caeea120f50cc5b626d4
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-09-18 20:03:40 -0500

    Funcionalidad de confirmacion de inicio de sesión / rechazo

--- app/src/main/java/com/rod/holaandroid/MainActivity.java
+++ app/src/main/java/com/rod/holaandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rod.holaandroid;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.rod.holaandroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String TAG = "MainActivity";
     private ActivityMainBinding binding;
 
     @Override
@@ -24,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });*/
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edUsername = binding.edTxtUsername;
+        EditText edPassword = binding.edTxtPassword;
+        Button btnLogin = binding.btnLogin;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edUsername.getText().toString().equals("admin") && edPassword.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Bienvenido/a a mi App", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Bienvenido/a a mi App");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error en la autenticación", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error en la autenticación");
+                }
+            }
+        });
+
     }
 }
